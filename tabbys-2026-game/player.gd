fedc13a6ff8352966577bb4045c7455013b5bed4
tahmed-sudo <tahmed@students.mags.school.nz>
2026-06-09 15:03:17 +1200

    Added a jump variable

--- tabbys-2026-game/player.gd
+++ tabbys-2026-game/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const GRAVITY = 980.0
+const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
@@ -12,6 +13,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
 	
 
 	move_and_slide()
