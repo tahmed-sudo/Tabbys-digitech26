8231d0ef86eee6d0085b118e9ad1feef4dcec2e9
tahmed-sudo <tahmed@students.mags.school.nz>
2026-06-19 09:35:06 +1200

    added movement and fine tuned enemy spawn

--- tabbys-2026-game/enemy.gd
+++ tabbys-2026-game/enemy.gd
@@ -1,1 +1,14 @@
 extends CharacterBody2D
+
+const enemy_SPEED = 150
+
+@onready var player = get_tree().get_first_node_in_group("Player")
+
+func _physics_process(delta: float) -> void:
+	velocity += get_gravity() * delta
+	move_and_slide()
+	
+	if player: 
+		var diretion = sign(player.global_position.x - global_position.x )
+		velocity.x = diretion * enemy_SPEED
+		
--- tabbys-2026-game/level.gd
+++ tabbys-2026-game/level.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 const JUMP = 400
-@onready var spawn_point = $Spawn_trigge/Markder2d
+
 @export var Enemy_scene: PackedScene = preload ("res://Enemy.tscn")
 
 var has_spawned: bool = false
@@ -9,11 +9,14 @@ var has_spawned: bool = false
 func _on_spawn_trigger_body_entered(body):
 	if body.is_in_group("Player") and not has_spawned:
 		call_deferred("spawn_enemy")
+		if body.is_in_group("Player") and !has_spawned:
+			has_spawned = true
+			call_deferred("spawn_enemy")
 		
 	
 func spawn_enemy() -> void:
-	var enemy = Enemy_scene.instantiate()
-	var spawn_offset = Marker2D
-	enemy.global_position = global_position + spawn_offset
-	get_tree().current_scene.add_child(enemy)
-	
+	var spawn_points = [ $Spawn_trigger/Marker2D, $Spawn_trigger/Marker2D2, $Spawn_trigger/Marker2D3]
+	for point in spawn_points:
+		var enemy = Enemy_scene.instantiate()
+		enemy.global_position = point.global_position
+		get_tree().current_scene.call_deferred("add_child", enemy)
